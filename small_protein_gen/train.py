863b164014b62df3a7d4de4cea3aa83f99476f76
matsagad <matsagad@gmail.com>
2025-03-06 07:55:34 +0000

    Add callbacks for debugging and model checkpointing; allow continued training from checkpoint

--- small_protein_gen/train.py
+++ small_protein_gen/train.py
@@ -8,7 +8,11 @@ def main(cfg: DictConfig) -> None:
     data: L.LightningDataModule = hydra.utils.instantiate(cfg.data)
     model: L.LightningModule = hydra.utils.instantiate(cfg.model)
     trainer: L.Trainer = hydra.utils.instantiate(cfg.trainer)
-    trainer.fit(model, data)
+
+    fit_kwargs = {}
+    if cfg.ckpt_path is not None:
+        fit_kwargs["ckpt_path"] = cfg.ckpt_path
+    trainer.fit(model, data, **fit_kwargs)
 
 
 if __name__ == "__main__":
