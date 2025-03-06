b52c3af08257fda01365adbdc4de76d7219fa1cb
matsagad <matsagad@gmail.com>
2025-03-06 13:39:55 +0000

    Fix incorrect variable name for file

--- scripts/get_cath_data.sh
+++ scripts/get_cath_data.sh
@@ -14,7 +14,7 @@ if [ -d $UNZIP_FOLDER ]; then echo "Data folder already exists."; exit 0; fi
 # Download and extract tar file.
 if [ ! -f $TAR_FILE ]; then echo "Downloading tar file..."; curl -o $TAR_FILE $CATH_ENDPOINT; else echo "Existing tar file found."; fi
 echo "Extracting tar file..."; tar -xzf $TAR_FILE --directory $DATA_PATH && $CLEAN && rm $TAR_FILE
-for FILE in $UNZIP_FOLDER/*; do mv $file $file.pdb; done
+for FILE in $UNZIP_FOLDER/*; do mv $FILE $FILE.pdb; done
 
 # Filter proteins to have length between 40 and 100.
 python3 scripts/filter_cath_data.py --data_folder $UNZIP_FOLDER --out_folder $FILTERED_FOLDER
