b5f3c60e6d03f9d55edd85057676ce3bc4948959
matsagad <matsagad@gmail.com>
2025-03-10 13:11:23 +0000

    Add PDB export method to structures

--- small_protein_gen/protein/structure.py
+++ small_protein_gen/protein/structure.py
@@ -1,9 +1,9 @@
-from biotite.structure import array, Atom, dihedral
+from biotite.structure import array, Atom, AtomArray, dihedral, stack
 from biotite.structure.io.pdb import PDBFile
 from small_protein_gen.utils.data import _place_atom
 import torch
 from torch import Tensor
-from typing import Dict
+from typing import Dict, List
 
 
 class ProteinStructure:
@@ -42,21 +42,17 @@ class ProteinStructure:
         )
         self.atom_pos["O"] = O
 
-    def to_pdb(self, file: str, impute_oxygen: bool = True) -> None:
+    def to_atom_array(self) -> AtomArray:
         N_RES = self.atom_pos["CA"].shape[0]
         BB_ATOMS = ["N", "CA", "C", "O"]
         ATOM_TO_ELEMENT = {"N": "N", "CA": "C", "C": "C", "O": "O"}
-
-        if impute_oxygen:
-            self._impute_oxygen()
-
-        out = []
+        arr = []
         for i in range(N_RES):
             res_id = i + 1
             for atom in BB_ATOMS:
                 if atom not in self.atom_pos or i >= len(self.atom_pos[atom]):
                     continue
-                out.append(
+                arr.append(
                     Atom(
                         self.atom_pos[atom][i].numpy(),
                         chain_id="A",
@@ -67,7 +63,32 @@ class ProteinStructure:
                         element=ATOM_TO_ELEMENT[atom],
                     )
                 )
-        out = array(out)
+        arr = array(arr)
+        return arr
+
+    def to_pdb(self, file: str, impute_oxygen: bool = True) -> None:
+        if impute_oxygen:
+            self._impute_oxygen()
+        arr = self.to_atom_array()
         f = PDBFile()
-        f.set_structure(out)
+        f.set_structure(arr)
         f.write(file)
+
+    def join_to_pdb(
+        structs: List["ProteinStructure"], file: str, impute_oxygen: bool = True
+    ) -> None:
+
+        f = PDBFile()
+        arr_stack = []
+        for struct in structs:
+            if impute_oxygen:
+                struct._impute_oxygen()
+            arr_stack.append(struct.to_atom_array())
+        arr_stack = stack(arr_stack)
+        f.set_structure(arr_stack)
+        f.write(file)
+
+    def center_at_origin(self) -> None:
+        offset = self.atom_pos["CA"].mean(dim=0)
+        for atom in self.atom_pos:
+            self.atom_pos[atom] -= offset
